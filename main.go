a79bc8b242516a33fd6b6fe57537bf1d0310bc8a
Satsuki Minato <minato@tieste.com>
2025-11-13 00:19:06 +0900

    アナログスティック範囲調整

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 	"machine/usb/hid/joystick"
 	"time"
@@ -17,7 +18,6 @@ var def = joystick.Definitions{
 }
 
 // var js = joystick.UseSettings(def, nil, nil, nil)
-
 var js = joystick.Port()
 
 func main() {
@@ -41,44 +41,54 @@ func main() {
 	analogY := machine.ADC{Pin: machine.D27}
 	analogY.Configure(machine.ADCConfig{})
 
-	for {
+	ticker := time.NewTicker(100 * time.Millisecond)
+	cnt := 0
+	const f = 3.0
+
+	for range ticker.C {
 		if !button1.Get() {
 			// println("button up is pressed!!")
 			js.SetHat(0, joystick.HatUp)
-		}
-		if !button2.Get() {
+		} else if !button2.Get() {
 			// println("button left is pressed!!")
 			js.SetHat(0, joystick.HatLeft)
-		}
-		if !button3.Get() {
+		} else if !button3.Get() {
 			// println("button right is pressed!!")
 			js.SetHat(0, joystick.HatRight)
-		}
-		if !button4.Get() {
+		} else if !button4.Get() {
 			// println("button down is pressed!!")
 			js.SetHat(0, joystick.HatDown)
-		}
-		if !button5.Get() {
-			// println("button a is pressed!!")
-			js.SetButton(0, true)
 		} else {
-			js.SetButton(0, false)
-		}
-		if !button6.Get() {
-			// println("button b is pressed!!")
-			js.SetButton(1, true)
-		} else {
-			js.SetButton(1, false)
+			js.SetHat(0, joystick.HatCenter)
 		}
 
+		// if !button5.Get() {
+		// 	// println("button a is pressed!!")
+		// 	js.SetButton(0, true)
+		// } else {
+		// 	js.SetButton(0, false)
+		// }
+		js.SetButton(0, !button5.Get())
+		// if !button6.Get() {
+		// 	// println("button b is pressed!!")
+		// 	js.SetButton(1, true)
+		// } else {
+		// 	js.SetButton(1, false)
+		// }
+		js.SetButton(1, !button6.Get())
+
 		x := analogX.Get()
+
+		// t := float64(cnt) * 0.01
+		// x := 32767 * math.Sin(2*math.Pi*f*t)
 		y := analogY.Get()
-		// fmt.Printf("%04X %04X\n", x, y)
-		js.SetAxis(0, int(x))
-		js.SetAxis(1, int(y))
+		fmt.Printf("%0d %0d\n", int(x), int(y))
+		js.SetAxis(0, int(x)-32767)
+		js.SetAxis(1, 32767-int(y))
 
 		js.SendState()
 
-		time.Sleep(time.Millisecond * 100)
+		// time.Sleep(time.Millisecond * 100)
+		cnt++
 	}
 }
