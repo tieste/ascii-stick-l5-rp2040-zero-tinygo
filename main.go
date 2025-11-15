508bd54cdde6c24fce35cf9f8d673570ed3726c0
Satsuki Minato <minato@tieste.com>
2025-11-15 15:15:52 +0900

    ピン番号変更
    あとボタンが2個足りなかったので追加

--- main.go
+++ main.go
@@ -34,42 +34,63 @@ func main() {
 	if js == nil {
 		panic("Failed to configure joystick")
 	}
-	button1 := machine.D3
-	button1.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	button2 := machine.D4
-	button2.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	button3 := machine.D5
-	button3.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	button4 := machine.D6
-	button4.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	button5 := machine.D15
-	button5.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
-	button6 := machine.D26
-	button6.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	hat1 := machine.D3
+	hat1.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	// hat1.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	hat2 := machine.D4
+	hat2.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	// hat2.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	hat3 := machine.D5
+	hat3.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	// hat3.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	hat4 := machine.D6
+	hat4.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	// hat4.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	button1 := machine.D7
+	// button1.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	button1.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	button2 := machine.D8
+	// button2.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	button2.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	button3 := machine.D9
+	// button3.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	button3.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	button4 := machine.D10
+	// button4.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	button4.Configure(machine.PinConfig{Mode: machine.PinInput})
 
 	machine.InitADC()
-	analogX := machine.ADC{Pin: machine.D28}
+	analogX := machine.ADC{Pin: machine.D27}
 	analogX.Configure(machine.ADCConfig{})
-	analogY := machine.ADC{Pin: machine.D27}
+	analogY := machine.ADC{Pin: machine.D28}
 	analogY.Configure(machine.ADCConfig{})
 
 	ticker := time.NewTicker(100 * time.Millisecond)
 
 	for range ticker.C {
-		if !button1.Get() {
+		if !hat1.Get() {
 			js.SetHat(0, joystick.HatUp)
-		} else if !button2.Get() {
+		} else if !hat2.Get() {
 			js.SetHat(0, joystick.HatLeft)
-		} else if !button3.Get() {
+		} else if !hat3.Get() {
 			js.SetHat(0, joystick.HatRight)
-		} else if !button4.Get() {
+		} else if !hat4.Get() {
 			js.SetHat(0, joystick.HatDown)
 		} else {
 			js.SetHat(0, joystick.HatCenter)
 		}
 
-		js.SetButton(0, !button5.Get())
-		js.SetButton(1, !button6.Get())
+		js.SetButton(0, !button1.Get())
+		js.SetButton(1, !button2.Get())
+		js.SetButton(2, !button3.Get())
+		js.SetButton(3, !button4.Get())
 
 		x := analogX.Get()
 		js.SetAxis(0, int(x)-32767)
